ae30b560dafd9ac2102f4ffded92764b5e9a7110
Chirag151102 <chiragparmar27806@gmail.com>
2025-07-16 13:35:21 +0530

    Delete My Portfolio directory

--- My Portfolio/script.js
+++
@@ -1,6 +0,0 @@
-function submitForm(event) {
-  event.preventDefault();
-  const name = document.getElementById("name").value;
-  alert("Thank you, " + name + "! Your message has been sent.");
-  document.querySelector("form").reset();
-}
